4b24995c1b17d8f510c80f6fbe4f07fbb7004777
Samuel Taylor <samtaylor.uk@gmail.com>
2016-02-18 15:32:11 +0000

    Added code

---
+++ 14.04/docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eo pipefail
+
+#Settings
+SCRIPTS_PATH=/app/firstrun
+
+if [ ! -e '/app/configured' ]; then
+    # Run Backup Script
+    while read FILE
+    do
+        NAME="${FILE##*/}"
+        echo "#### Running first run script: $NAME"
+        /bin/bash $FILE
+    done < <( find $SCRIPTS_PATH/* -maxdepth 0 -type f 
+    
+    touch /app/configured
+fi
+
+exec "$@"
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # docker-base
+
+[![](https://badge.imagelayers.io/samtayuk/base:latest.svg)](https://imagelayers.io/?images=samtayuk/base:latest 'Get your own badge on imagelayers.io')
